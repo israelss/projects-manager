20b7e1c9313857caa37756ef7c90d70b5e1f2fa5
Israel Sant'Anna <israel.santanna@gmail.com>
2022-11-26 20:25:19 -0300

    refactor(services/projects): extract logic out of `get` function

--- api/src/services/project.ts
+++ api/src/services/project.ts
@@ -4,10 +4,40 @@ import axios from 'axios'
 import { ClientError } from '../enums/http_status_codes'
 
 type NewProjectData = Pick<Project, 'title' | 'zip_code' | 'deadline' | 'cost'>
-type ProjectData = Omit<Project, 'zip_code'> & { location: string }
+type ProjectWithLocation = Project & { location: string }
+type ProjectData = Omit<ProjectWithLocation, 'zip_code'>
 interface CepData {
   city: string
   state: string
+}
+
+const includeLocation = async (project: Project): Promise<ProjectWithLocation> => {
+  const cep = project.zip_code
+  let location = `Não foi possível encontrar a localização do CEP ${cep}`
+  try {
+    const CEP_API_URL = `https://brasilapi.com.br/api/cep/v2/${cep}`
+    const headers = { 'Accept-Encoding': 'application/json' }
+
+    const response = await axios.get<CepData>(CEP_API_URL, { headers })
+    if (response.status !== ClientError.NOT_FOUND) {
+      const { city, state } = response.data
+      location = `${city}/${state}`
+    }
+  } catch (error) {
+    console.error(error)
+  }
+
+  return { location, ...project }
+}
+
+const removeZipCode = (project: Partial<Project>): ProjectData => {
+  delete project.zip_code
+  return project as Required<ProjectData>
+}
+
+const transformCepToLocation = async (project: Project): Promise<ProjectData> => {
+  const projectWithLocation = await includeLocation(project)
+  return removeZipCode(projectWithLocation)
 }
 
 export const create = async (username: string, project: NewProjectData): Promise<void> => {
@@ -20,30 +50,7 @@ export const create = async (username: string, project: NewProjectData): Promise
 }
 
 export const get = async (id: string): Promise<ProjectData | null> => {
-  const projectStored: Partial<Project | null> = await db.project.findUnique({ where: { id } })
-  if (projectStored === null) return null
-  const cep = projectStored.zip_code as number
-  let location: string = `Não foi possível encontrar a localização do CEP ${cep}`
-  try {
-    const CEP_API_URL = `https://brasilapi.com.br/api/cep/v2/${cep}`
-    const headers = {
-      'Accept-Encoding': 'application/json'
-    }
-    const response = await axios.get<CepData>(CEP_API_URL, { headers })
-    if (response.status !== ClientError.NOT_FOUND) {
-      const { city, state } = response.data
-      location = `${city}/${state}`
-    }
-  } catch (error) {
-    console.error(error)
-  }
-
-  const project = {
-    location,
-    ...projectStored
-  }
-
-  delete project.zip_code
-
-  return project as Required<ProjectData>
+  const project = await db.project.findUnique({ where: { id } })
+  if (project === null) return null
+  return await transformCepToLocation(project)
 }
