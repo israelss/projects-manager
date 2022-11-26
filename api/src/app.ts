d6469d7e73d79d02d53e96827b342b16c044d10f
Israel Sant'Anna <israel.santanna@gmail.com>
2022-11-26 14:44:42 -0300

    feat(app): use /users route and error middleware

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,9 +1,13 @@
 import express from 'express'
+import { errorMiddleware } from './middlewares'
+import usersRoute from './routes/users'
 
 const app = express()
 
 app.use(express.json())
 
-app.get('/', (_, res) => res.status(200).json({ message: 'Success!' }))
+app.use('/users', usersRoute)
+
+app.use(errorMiddleware.handle)
 
 export default app
