11515f976c4bd548417c385f59077523aef741d8
Israel Sant'Anna <israel.santanna@gmail.com>
2022-11-26 20:27:01 -0300

    feat(app): use /projects route

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,12 +1,14 @@
 import express from 'express'
 import { errorMiddleware } from './middlewares'
 import projectRoute from './routes/project'
+import projectsRoute from './routes/projects'
 import usersRoute from './routes/users'
 
 const app = express()
 
 app.use(express.json())
 
+app.use('/projects', projectsRoute)
 app.use('/project', projectRoute)
 app.use('/users', usersRoute)
 
