ba6cbf00c41f670de974e461eb971acf44476ab5
Israel Sant'Anna <israel.santanna@gmail.com>
2022-11-26 16:53:24 -0300

    feat(utils/customError): create errors for project

--- api/src/utils/customError/errors.ts
+++ api/src/utils/customError/errors.ts
@@ -16,5 +16,25 @@ export const BadRequest = {
   passwordInvalid: {
     code: ClientError.BAD_REQUEST,
     message: 'Password must have at least 8 (eight) characters'
-  }
+  },
+  titleInvalid: {
+    code: ClientError.BAD_REQUEST,
+    message: 'Title must have at least 4 (four) characters'
+  },
+  zipCodeInvalid: {
+    code: ClientError.BAD_REQUEST,
+    message: 'Zip Code must have exactly 8 (eight) numbers'
+  },
+  costInvalid: {
+    code: ClientError.BAD_REQUEST,
+    message: 'Cost must have at least 1 (one) number'
+  },
+  deadlineInvalid: {
+    code: ClientError.BAD_REQUEST,
+    message: 'Deadline must be a valid DateTime value'
+  },
+  headerInvalid: {
+    code: ClientError.BAD_REQUEST,
+    message: 'Username must be provided on header'
+  },
 }
