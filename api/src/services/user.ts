316d0a42ee4ff80265678b905971665d78b7b36e
Israel Sant'Anna <israel.santanna@gmail.com>
2022-11-27 13:10:33 -0300

    feat(services): add user login service

--- api/src/services/user.ts
+++ api/src/services/user.ts
@@ -1,6 +1,11 @@
 import { User } from '@prisma/client'
 import { db } from '../utils/db.server'
 
+interface LoginInput {
+  username: string
+  password: string
+}
+
 export const create = async (user: User): Promise<string> => {
   const { username } = await db.user.create({
     data: user
@@ -12,3 +17,13 @@ export const find = async (username: string): Promise<User | null> => await db
   .user.findUnique({
     where: { username }
   })
+
+export const login = async (userData: LoginInput): Promise<string | null> => {
+  const { username, password } = userData
+  const user = await db.user.findUnique({
+    where: { username }
+  })
+
+  if (user === null || user.password !== password) return null
+  return user.username
+}
