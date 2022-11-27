2c05ea54e45b4ab6e9c4babb960ba8f37ed9c588
Israel Sant'Anna <israel.santanna@gmail.com>
2022-11-27 17:38:16 -0300

    test(services/user): create mocks

---
+++ api/tests/unit/mocks/user.ts
@@ -0,0 +1,12 @@
+export const userOneInput = {
+  name: 'User One',
+  username: 'user.one',
+  password: '12345678'
+}
+
+export const userOneCreated = {
+  id: '1a2b3c',
+  name: 'User One',
+  username: 'user.one',
+  password: '12345678'
+}
