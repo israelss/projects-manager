597bf04eea0953a5f78944de320bb597ee126160
Israel Sant'Anna <israel.santanna@gmail.com>
2022-11-28 17:25:19 -0300

    fix(controllers/project): fix handlers signatures

--- api/src/controllers/project.ts
+++ api/src/controllers/project.ts
@@ -1,41 +1,41 @@
-import { Response, Request } from 'express'
+import { Response, Request, NextFunction } from 'express'
 import { Success } from '../enums/http_status_codes'
 import { projectService } from '../services'
 
-export const create = async (req: Request, res: Response): Promise<Response> => {
+export const create = async (req: Request, res: Response, _next: NextFunction): Promise<void> => {
   const project = req.body
   const { username } = req.headers
   const createdProject = await projectService.create(username as string, project)
-  return res.status(Success.CREATED).json(createdProject)
+  res.status(Success.CREATED).json(createdProject)
 }
 
-export const get = async (req: Request, res: Response): Promise<Response> => {
+export const get = async (req: Request, res: Response, _next: NextFunction): Promise<void> => {
   const { id } = req.params
   const project = await projectService.get(id)
-  return res.status(Success.OK).json(project)
+  res.status(Success.OK).json(project)
 }
 
-export const getAll = async (req: Request, res: Response): Promise<Response> => {
+export const getAll = async (req: Request, res: Response, _next: NextFunction): Promise<void> => {
   const { username } = req.headers
   const projects = await projectService.getAll(username as string)
-  return res.status(Success.OK).json({ projects })
+  res.status(Success.OK).json({ projects })
 }
 
-export const markAsDone = async (req: Request, res: Response): Promise<Response> => {
+export const markAsDone = async (req: Request, res: Response, _next: NextFunction): Promise<void> => {
   const { id } = req.params
   const updatedProject = await projectService.markAsDone(id)
-  return res.status(Success.OK).json(updatedProject)
+  res.status(Success.OK).json(updatedProject)
 }
 
-export const remove = async (req: Request, res: Response): Promise<Response> => {
+export const remove = async (req: Request, res: Response, _next: NextFunction): Promise<void> => {
   const { id } = req.params
   await projectService.remove(id)
-  return res.status(Success.NO_CONTENT).end()
+  res.status(Success.NO_CONTENT).end()
 }
 
-export const update = async (req: Request, res: Response): Promise<Response> => {
+export const update = async (req: Request, res: Response, _next: NextFunction): Promise<void> => {
   const projectData = req.body
   const { id } = req.params
   const updatedProject = await projectService.update(id, projectData)
-  return res.status(Success.OK).json(updatedProject)
+  res.status(Success.OK).json(updatedProject)
 }
