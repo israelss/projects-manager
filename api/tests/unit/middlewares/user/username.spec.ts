db777510e927e6d70266795adc64d0c23d5e08f8
Israel Sant'Anna <israel.santanna@gmail.com>
2022-11-28 15:00:44 -0300

    test(middlewares/user): add test to username

---
+++ api/tests/unit/middlewares/user/username.spec.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from 'express'
+import { afterEach, describe, expect, test, vi } from 'vitest'
+import usernameValidator from '../../../../src/middlewares/user/username'
+import CustomError, { BadRequest } from '../../../../src/utils/customError'
+
+afterEach(() => {
+  vi.clearAllMocks()
+})
+
+describe('User', () => {
+  describe('Middleware', () => {
+    describe('username', () => {
+      test('should pass with a valid username', async () => {
+        const req = {
+          body: {
+            username: 'user.one'
+          }
+        } as Request
+        const res = {} as Response
+        const next = vi.fn()
+
+        usernameValidator(req, res, next)
+
+        expect(next).toBeCalledWith()
+      })
+      test('should call next with error with an invalid username', async () => {
+        const req = {
+          body: {
+            username: 'one'
+          }
+        } as Request
+        const res = {} as Response
+        const next = vi.fn()
+
+        usernameValidator(req, res, next)
+
+        expect(next).toBeCalledWith(new CustomError(BadRequest.usernameInvalid))
+      })
+    })
+  })
+})
