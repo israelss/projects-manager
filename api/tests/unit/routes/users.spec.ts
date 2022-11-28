2a9008d63962f7f5d1627fca28fd4e38efa7c911
Israel Sant'Anna <israel.santanna@gmail.com>
2022-11-28 13:46:34 -0300

    test(routes/users): add test for POST /users

--- api/tests/unit/routes/users.spec.ts
+++ api/tests/unit/routes/users.spec.ts
@@ -1,4 +1,4 @@
-import { afterEach, describe, expect, test, vi } from 'vitest'
+import { afterAll, afterEach, describe, expect, test, vi } from 'vitest'
 import request from 'supertest'
 import { userController } from '../../../src/controllers'
 import { userValidation } from '../../../src/middlewares'
@@ -6,14 +6,21 @@ import { emptyHandler, emptyHandlerAsync } from '../mocks/middlewares'
 
 // Any handler mock implementation must be before the import of app,
 // otherwise the handler real implementation is used instead of the mocked one
+vi.spyOn(userController, 'create').mockImplementation(emptyHandlerAsync)
 vi.spyOn(userController, 'login').mockImplementation(emptyHandlerAsync)
 vi.spyOn(userValidation, 'username').mockImplementation(emptyHandler)
+vi.spyOn(userValidation, 'usernameUniqueness').mockImplementation(emptyHandlerAsync)
+vi.spyOn(userValidation, 'name').mockImplementation(emptyHandler)
 vi.spyOn(userValidation, 'password').mockImplementation(emptyHandler)
 
 import app from '../../../src/app' // eslint-disable-line import/first
 
 afterEach(() => {
-  vi.restoreAllMocks()
+  vi.clearAllMocks()
+})
+
+afterAll(() => {
+  vi.resetAllMocks()
 })
 
 describe('User', () => {
@@ -25,6 +32,17 @@ describe('User', () => {
         expect(userValidation.username).toHaveBeenCalledOnce()
         expect(userValidation.password).toHaveBeenCalledOnce()
         expect(userController.login).toHaveBeenCalledOnce()
+      })
+    })
+    describe('POST /', () => {
+      test('should call all middlewares and right controller method', async () => {
+        await request(app).post('/users')
+
+        expect(userValidation.username).toHaveBeenCalledOnce()
+        expect(userValidation.usernameUniqueness).toHaveBeenCalledOnce()
+        expect(userValidation.name).toHaveBeenCalledOnce()
+        expect(userValidation.password).toHaveBeenCalledOnce()
+        expect(userController.create).toHaveBeenCalledOnce()
       })
     })
   })
