e187af1daaecdfbe1afe5d79ab2f53d3a49084c1
Israel Sant'Anna <israel.santanna@gmail.com>
2022-11-28 21:34:07 -0300

    feat: enable CORS

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import { errorMiddleware } from './middlewares'
 import projectRoute from './routes/project'
 import projectsRoute from './routes/projects'
@@ -7,6 +8,7 @@ import usersRoute from './routes/users'
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/projects', projectsRoute)
 app.use('/project', projectRoute)
