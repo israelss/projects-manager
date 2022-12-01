07249d89ae9e4c1a0a1e97d79378baee14fa90e1
Israel Sant'Anna <israel.santanna@gmail.com>
2022-12-01 19:44:30 -0300

    feat: add dockerized dev setup

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    port: 3000,
+    host: true,
+    strictPort: true,
+    hmr: {
+      port: 4000
+    }
+  }
 })
