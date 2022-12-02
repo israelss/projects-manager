f28119d0e77b91b6689c8c965fca6de36c4edd1c
Israel Sant'Anna <israel.santanna@gmail.com>
2022-12-02 13:39:39 -0300

    fix: block unauthenticated calls

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import { errorMiddleware } from './middlewares'
+import { errorMiddleware, projectValidation } from './middlewares'
 import projectRoute from './routes/project'
 import projectsRoute from './routes/projects'
 import usersRoute from './routes/users'
@@ -10,9 +10,11 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.use('/users', usersRoute)
+
+app.use(projectValidation.username)
 app.use('/projects', projectsRoute)
 app.use('/project', projectRoute)
-app.use('/users', usersRoute)
 
 app.use(errorMiddleware.handle)
 
